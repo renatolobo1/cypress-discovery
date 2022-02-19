0c4cd56744e3bf792a08c5c4dbb927ecf12e6fc2
Renato Lobo <renatolobo_@hotmail.com>
2022-02-19 20:36:08 -0300

    Entendendo os ganchos do Cypress

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,6 +1,24 @@
 import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro', () =>{
+
+// before(function() {
+//   cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes')
+// })
+
+// beforeEach(function(){
+//   cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
+// })
+
+// after(function() {
+//   cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes')
+// })
+
+// afterEach(function(){
+//   cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
+// })
+
+
   it('Seja um entregador', () =>{   
 
     var deliver ={
